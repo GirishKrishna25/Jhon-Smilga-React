b614c98f5d89353c0a89bfa5df4010be1669f617
Girish Krishna <i.girish.krishna@gmail.com>
2022-11-08 09:01:05 +0000

    Done with useRef

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,35 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
+// to set up uncontrolled inputs
+// works lot like useState
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const inputRef = useRef(null);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    alert(inputRef.current.value);
+  };
+
+  // after reload the pointer focus will be at the input field.
+  useEffect(() => {
+    console.log(inputRef.current);
+    inputRef.current.focus();
+  });
+
+  return (
+    <>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <input type="text" ref={inputRef} />
+          <button type="submit">submit</button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default UseRefBasics;
