4e1816c11395a54e70eb5db19721801567b7f8bd
GirishKrishna25 <82050669+GirishKrishna25@users.noreply.github.com>
2022-11-11 13:54:35 +0530

    5 files modified

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// stackblitz not finding this. don't know why.
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = (props) => {
+  const { name, price, image } = props.product;
+  const url = image && image.url; // otherwise we can't use 'defaultImage'
+  return (
+    <article className="product">
+      {/* this one throws an error because one of the product object doesn't contain image key at all.
+      so we use 'prop-types' and 'defaultProps' to handle. */}
+      <img src={url || defaultImage} alt={name || 'default name'} />
+      <h4>{name}</h4>
+      <p>${price || 3.99}</p>
+    </article>
+  );
 };
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.object.isRequired,
+};
+
+// we replaced this by using '||' operator
+// Product.defaultProps = {
+//   name: 'default name',
+//   price: 3.05,
+//   image: defaultImage,
+// };
 
 export default Product;
--- src/tutorial/10-prop-types/setup/index.js
+++ src/tutorial/10-prop-types/setup/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Product from './Product'
 import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-prop-types-example'
 
 const Index = () => {
@@ -13,7 +11,7 @@ const Index = () => {
       <h2>products</h2>
       <section className='products'>
         {products.map((product) => {
-          return <Product key={product.id} {...product} />
+          return <Product key={product.id} product={product} />
         })}
       </section>
     </div>
