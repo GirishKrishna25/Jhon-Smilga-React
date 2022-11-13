36c6fbf9eae05f0ea06ab6032f248170994324b9
GirishKrishna25 <82050669+GirishKrishna25@users.noreply.github.com>
2022-11-13 19:15:00 +0530

    6 files modified

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -10,7 +10,31 @@ import Person from './Person';
 // navbar
 import Navbar from './Navbar';
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return (
+    <Router>
+      {/* Navbar present in every page */}
+      <Navbar />
+      {/* due to the 'Switch' first matched page only opens. 
+      this is important otherwise 'Error' page opens in all pages */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/people">
+          <People />
+        </Route>
+        {/* when we click on a product, it opens up a new page.
+        this is how it does. It's ok to use "/:id" */}
+        <Route path="/person/:id" children={<Person />}></Route>
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default ReactRouterSetup;
